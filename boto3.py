a24b40a04d6b8a20436fdb8459475296025da73f
Sean Wu <panda@ms5.url.com.tw>
2019-05-03 11:42:27 +0800

    Update boto3.py

--- boto3.py
+++ boto3.py
@@ -9,9 +9,9 @@ region=us-east-1
 [default]
 aws_access_key_id = YOUR_ACCESS_KEY
 aws_secret_access_key = YOUR_SECRET_KEY
-####
+#####
 
-
+### python program ###
 import boto3
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('weather')
