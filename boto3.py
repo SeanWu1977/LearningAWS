a71d5fb7b70e5821e4b9737bb1c6582ed630afa6
Sean Wu <panda@ms5.url.com.tw>
2019-05-03 11:44:50 +0800

    Update boto3.py

--- boto3.py
+++ boto3.py
@@ -1,15 +1,25 @@
 # python SDK to AWS
+pip install boto3
+
+##### linux
+~/.aws/config
+~/.aws/credentials
 
 ##### windows environment setting
 # AWS_CONFIG_FILE  = C:\tmp\aws\config
+# AWS_SHARED_CREDENTIALS_FILE = C:\tmp\aws\credentials
+
+# config file
 [default]
 region=us-east-1
 
-# AWS_SHARED_CREDENTIALS_FILE = C:\tmp\aws\credentials
+# credentials file
 [default]
 aws_access_key_id = YOUR_ACCESS_KEY
 aws_secret_access_key = YOUR_SECRET_KEY
-#####
+
+
+
 
 ### python program ###
 import boto3
